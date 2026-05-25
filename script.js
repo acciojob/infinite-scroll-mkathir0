b2c6ff4209ed03e93420aef1fdc9f02fa0c9a3ff
Mithrajit Kathir <mithrajit07@gmail.com>
2026-05-25 11:09:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,26 @@
-//your code here!
+let lists = document.getElementById("infi-list");
 
+let count = 1;
+
+// Function to add items
+function addItems(num) {
+    for (let i = 0; i < num; i++) {
+        let item = document.createElement("li");
+        item.innerText = "Item " + count;
+        lists.appendChild(item);
+        count++;
+    }
+}
+
+// Add initial 10 items
+addItems(10);
+
+// Detect scrolling to bottom
+lists.addEventListener("scroll", function () {
+    if (
+        lists.scrollTop + lists.clientHeight >=
+        lists.scrollHeight
+    ) {
+        addItems(2);
+    }
+});
